b839e6563ecde1265f7f243dbf56cc3f9d24da7d
ridz883 <ridhoahmadgembul@gmail.com>
2026-03-13 03:44:36 +0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Konfigurasi ini wajib ada agar Vercel tahu ini adalah aplikasi React
-export default defineConfig({
-  plugins: [react()],
-})
