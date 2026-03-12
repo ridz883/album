48371a9e7ccc41b7d9342e2c62fa9f6a06e82dc3
ridz883 <ridhoahmadgembul@gmail.com>
2026-03-12 23:35:05 +0700

    Create vite.config.js for React setup
    Add standard configuration for React Vite project.

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Konfigurasi standar untuk proyek React Vite
+export default defineConfig({
+  plugins: [react()],
+})
